b3fcedcc2181689a64e1c5116e3570b0b999cf30
Timothy Atobatele <Gbeminija.oladimeji@gmail.com>
2022-07-18 23:29:57 +0100

    Task 1-6

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+#function that prints "{:d}".format()
+
+def safe_print_integer(value):
+    """Print an integer with "{:d}".format().
+
+    Args:
+
+        value (int): The integer toprint
+
+    Returns:
+        If a Type Error or ValueError occurs - False.
+        Otherwise - True.
+    """
+    try:
+        Print("{:d}".format()(value))
+        return(True)
+    except (TypeError, ValueError):
+        return(False)
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# A function that raises name exception with message
+
+def raise_exception_msg(message=""):
+    rasieNmaeError(message)
