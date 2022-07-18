89d33468413cd2b4e630a4739f0baa2046633fd1
Timothy Atobatele <Gbeminija.oladimeji@gmail.com>
2022-07-19 00:01:52 +0100

    Task 1-6

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -2,5 +2,7 @@
 # A function that raises name exception with message
 
 def raise_exception_msg(message=""):
-    """Raise a NameError exception with a message."""
-    rasieNmaeError(message)
+    if NameError:
+        raise NameError(message)
+    else:
+        pass
