71b22b17f6ae4ddda1a11f34d1c9c8656bc99951
Timothy Atobatele <Gbeminija.oladimeji@gmail.com>
2022-08-02 20:47:35 +0100

    task0-14

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,11 +7,11 @@ if __name__ == "__main__":
     load_from_json_file = \
             __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    ls = load_from_json_file("add_item.json")
-except:
-    ls = []
+    try:
+        ls = load_from_json_file("add_item.json")
+    except:
+        ls = []
 
-for i in sys.argv[1:]:
-    ls.append(i)
-save_to_json_file(ls, "add_item.json")
+    for i in sys.argv[1:]:
+        ls.append(i)
+    save_to_json_file(ls, "add_item.json")
