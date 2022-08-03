8fbb6371e296311c056c4362d70d460d2ed9dac5
Timothy Atobatele <Gbeminija.oladimeji@gmail.com>
2022-08-03 11:22:17 +0100

    task0-14

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,7 +8,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
     ls = load_from_json_file("add_item.json")
-except:
+except FileNotFoundError:
     ls = []
 
 for i in sys.argv[1:]:
