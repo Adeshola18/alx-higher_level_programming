15045f48f75ad41e6fbd9adb92fdf88bf4fa7fba
Timothy Atobatele <gbeminija.oladimeji.com>
2022-10-10 01:37:06 +0100

    Task0-18

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
-# Displays all cities of a given state from the
-# states table of the database hbtn_0e_4_usa.
-# Safe from SQL injections.
-# Usage: ./5-filter_cities.py <mysql username> \
-#                             <mysql password> \
-#                             <database name> \
-#                             <state name searched>
-import sys
+"""
+Displays all cities of a given state from the
+states table of the database hbtn_0e_4_usa.
+Safe from SQL injections.
+Usage: ./5-filter_cities.py <mysql username> \
+                            <mysql password> \
+                            <database name> \
+                            <state name searched>
+"""
+
+from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         passwd=argv[2], port=3306,
+                         db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT `cities`.`name` FROM cities \
+                INNER JOIN `states` ON \
+                `cities`.`state_id` = `states`.`id` \
+                WHERE `states`.`name` = %s \
+                ORDER BY `cities`.`id` ASC", (argv[4], ))
+
+    print(", ".join([city[0] for city in cur.fetchall()]))
