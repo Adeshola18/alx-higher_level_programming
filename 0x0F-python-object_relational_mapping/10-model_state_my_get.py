e58e038ce4e172f76bc901a3956f75d0312785a2
Timothy Atobatele <gbeminija.oladimeji.com>
2022-10-13 07:23:48 +0100

    updating

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,27 +1,36 @@
 #!/usr/bin/python3
-# Lists the State object with the name passed as argument
-# from the database hbtn_0e_6_usa.
-# Usage: ./10-model_state_my_get.py <mysql username> /
-#                                   <mysql password> /
-#                                   <database name>
-#                                   <state name searched>
-import sys
+"""
+Write a script that lists all State objects that contains "a" from
+the database hbtn_0e_6_usa
+"""
+from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    search = argv[4]
+
+    if search.__contains__('TRUNCATE' or 'FROM' or 'SELECT' or '*'):
+        search = ''
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(user, passwd, db), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    # To start a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    found = False
-    for state in session.query(State):
-        if state.name == sys.argv[4]:
-            print("{}".format(state.id))
-            found = True
-            break
-    if found is False:
+    state = session.query(State).filter(State.name == search).first()
+
+    if state is not None:
+        print("{}".format(state.id))
+    else:
         print("Not found")
+
+    session.close()
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
-# Prints the first State object from the database hbtn_0e_6_usa.
-# Usage: ./8-model_state_fetch_first.py <mysql username> /
-#                                       <mysql password> /
-#                                       <database name>
-import sys
+"""
+Write a script that lists all State objects from
+the database hbtn_0e_6_usa
+"""
+from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(user, passwd, db), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    # To start a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
     state = session.query(State).order_by(State.id).first()
+
     if state is None:
         print("Nothing")
     else:
         print("{}: {}".format(state.id, state.name))
+    session.close()
