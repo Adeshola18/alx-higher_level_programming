14a447683d11972fb4fdd6e260e7083fcee46367
Timothy Atobatele <gbeminija.oladimeji.com>
2022-10-15 01:19:57 +0100

    updating

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 """
-a script that changes the name of a State
-object from the database hbtn_0e_6_usa
+a script that lists all City objects from the database hbtn_0e_101_usa
 """
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
-from model_state import Base, State
+from relationship_city import City
+from relationship_state import Base, State
 
 
 if __name__ == '__main__':
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}"
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
         .format(sys.argv[1], sys.argv[2], sys.argv[3])
     )
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    res = session.query(State).filter(State.id == 2).first()
-    res.name = 'New Mexico'
-    session.commit()
-    
+    resArr = session.query(City.id, City.name, State.name)
+    resFilteredArr = resArr.filter(City.state_id == State.id).order_by(City.id)
+    for r in resFilteredArr:
+        print("{}: {} -> {}".format(r[0], r[1], r[2]))
